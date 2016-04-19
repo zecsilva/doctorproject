45ec453e73a062b4e72637ae552069b6c5483dca
lrferr <lrferr@gmail.com>
2016-04-19 20:42:42 -0300

    Alteração da Ordem de Composição

--- workspace/EstruturaEstilo_versao2/src/estilo/util/Constantes.java
+++ workspace/EstruturaEstilo_versao2/src/estilo/util/Constantes.java
@@ -3,7 +3,7 @@ package estilo.util;
 public class Constantes {
 
 	public static final String ORDEM_COMPOSICAO_1 = "Introdução->Desenvolvimento->Resumo->Atividade->Avaliação";
-	public static final String ORDEM_COMPOSICAO_2 = "Introdução->Desenvolvimento->Atividade->Avaliação->Resumo";
+	public static final String ORDEM_COMPOSICAO_2 = "Introdução->Desenvolvimento->Atividade->Resumo->Avaliação";
 	public static final String ORDEM_COMPOSICAO_3 = "Resumo->Introdução->Desenvolvimento->Atividade->Avaliação";
 	
 	public static final String ORDEM_DETALHAMENTO_1 = "Específico Para Geral";
