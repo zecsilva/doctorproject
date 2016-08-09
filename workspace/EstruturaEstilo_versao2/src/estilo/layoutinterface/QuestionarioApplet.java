38b82289b207272da1cd13f28899572617932673
lrferr <lrferr@gmail.com>
2016-08-08 22:00:40 -0300

    testes

--- workspace/EstruturaEstilo_versao2/src/estilo/layoutinterface/QuestionarioApplet.java
+++ workspace/EstruturaEstilo_versao2/src/estilo/layoutinterface/QuestionarioApplet.java
@@ -1,7 +1,22 @@
 package estilo.layoutinterface;
 
+import java.awt.Component;
+
 import javax.swing.JApplet;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class QuestionarioApplet extends JApplet {
-
+	JScrollPane scrollPane;
+	
+	public QuestionarioApplet() {
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 731, 462);
+		getContentPane().add(panel);
+		scrollPane = new JScrollPane((Component) null);
+		scrollPane.setBounds(189, 190, 469, 250);
+		panel.add(scrollPane);
+	}
 }
